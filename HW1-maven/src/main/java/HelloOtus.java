bbb7a1c60dd80213fa853c5abadb5312b7514233
Dmitry Konoplev <konoplev88@mail.ru>
2019-09-28 17:47:03 +1000

    commit

--- HW1-maven/src/main/java/HelloOtus.java
+++ HW1-maven/src/main/java/HelloOtus.java
@@ -1,4 +1,16 @@
 package main.java;
 
+import com.google.common.base.Joiner;
+
+import java.util.ArrayList;
+
 public class HelloOtus {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 1; i < 100; i++) {
+            list.add(String.valueOf(i));
+        }
+        String joiner = Joiner.on(") ").skipNulls().join(list);
+        System.out.println(joiner);
+    }
 }
