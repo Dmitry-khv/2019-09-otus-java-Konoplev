d53f9d2cb076030acc10a9dfdc889794623fcb59
Dmitry Konoplev <konoplev88@mail.ru>
2019-10-14 01:41:20 +1000

    adding hw02-generics

--- hw02-generics/src/main/java/ru/otus/hw02/DIYarrayList.java
+++ hw02-generics/src/main/java/ru/otus/hw02/DIYarrayList.java
@@ -1,4 +1,223 @@
 package ru.otus.hw02;
 
-public class DIYarrayList {
+
+import java.util.*;
+
+public class DIYarrayList<T> implements List<T> {
+    private Object[] array;
+    private int capacity = 0;
+    private final int INIT_SIZE = 10;
+    private int collectionCopySize;
+
+    public DIYarrayList() {
+        this.array = new Object[INIT_SIZE];
+    }
+
+    public DIYarrayList(int setSize) {
+        this.array = new Object[setSize];
+    }
+
+    public DIYarrayList (DIYarrayList<T> list) {
+        this.array = list.toArray();
+        collectionCopySize = array.length;
+    }
+
+    public int arraySize() {
+        return array.length;
+    }
+
+    private Object[] changeArraySize() {
+        return Arrays.copyOf(array, arraySize() * 2);
+    }
+
+    @Override
+    public int size() {
+        return collectionCopySize > 0 ? collectionCopySize : capacity;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(array, capacity);
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean add(T t) {
+        boolean result = false;
+        if (t != null) {
+            if (capacity >= arraySize())
+                array = changeArraySize();
+            array[capacity] = t;
+            capacity++;
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size())
+            throw new IndexOutOfBoundsException();
+        return (T) array[index];
+    }
+
+    @Override
+    public T set(int index, T element) {
+        Object prevElement = array[index];
+        array[index] = element;
+        capacity = index + 1;
+        return (T) prevElement;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return new ListIterator<T>() {
+            private int currentIndex = -1;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < capacity;
+            }
+
+            @Override
+            public T next() {
+                if (currentIndex < size()) {
+                    int tmp = currentIndex + 1;
+                    currentIndex++;
+                    return (T) array[tmp];
+                }
+                return null;
+            }
+
+            @Override
+            public boolean hasPrevious() {
+                return false;
+            }
+
+            @Override
+            public T previous() {
+                return null;
+            }
+
+            @Override
+            public int nextIndex() {
+                return 0;
+            }
+
+            @Override
+            public int previousIndex() {
+                return 0;
+            }
+
+            @Override
+            public void remove() {
+
+            }
+
+            @Override
+            public void set(T t) {
+                array[currentIndex] = t;
+//                currentIndex++;
+            }
+
+            @Override
+            public void add(T t) {
+
+            }
+        };
+//        return listIterator;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+
 }
+
+
+
+
+
--- hw02-generics/src/test/java/ru/otus/hw02/DIYarrayListTest.java
+++ hw02-generics/src/test/java/ru/otus/hw02/DIYarrayListTest.java
@@ -1,23 +1,56 @@
 package ru.otus.hw02;
 
-
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
-public class DIYarrayTest {
+public class DIYarrayListTest {
     @Test
     void addAll() {
-        DIYarrayList<Integer> list = new DIYarrayList<>();
+        DIYarrayList<Integer> list = new DIYarrayList<>(15);
 
-        Collections.addAll(list, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30);
+        Collections.addAll(list, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+
+    @Test
+    void sort() {
+        DIYarrayList<Character> list = new DIYarrayList<>();
+        Collections.addAll(list, 'z', 'x', 'y', 'r', 't', 'd', 'f', 'g', 'e', 'p', 'l', 'q', 's', 'j', 'b', 'm', 'n', 'v', 'w', 'y', 'u', 'i');
+
+//        DIYarrayList<Integer> list = new DIYarrayList<>();
+//        Collections.addAll(list, 22,666,31,26,861,265,15,1,23,864,5,431,5,66,99,12,23,45,46,53,54,65,54,98,46,28,99,87,54,61);
+
+
+        Collections.sort(list, null);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
     }
 
     @Test
     void copy() {
-        DIYarrayList<Integer> list = new DIYarrayList<>();
+//        ArrayList<Integer> src = new ArrayList<>();
+//        Collections.addAll(src, 22,666,31,26,861,265,15,1,23,864,5,431,5,66,99,12,23,45,46,53,54,65,54,98,46,28,99,87,54,61);
+//        ArrayList<Integer> dest = new ArrayList<>(src);
 
-        Collections.sort(list, null);
+        DIYarrayList<Integer> src = new DIYarrayList<>();
+        Collections.addAll(src, 22,666,31,26,861,265,15,1,23,864,5,431,5,66,99,12,23,45,46,53,54,65,54,98,46,28,99,87,54,61);
+        DIYarrayList<Integer> dest = new DIYarrayList<>(src);
+
+        Collections.copy(dest, src);
+
+        for (int i = 0; i < src.size(); i++) {
+            System.out.print(src.get(i) + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < dest.size(); i++) {
+            System.out.print(dest.get(i) + " ");
+        }
 
     }
 
